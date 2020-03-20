230953518e673486414a014354affccb9c8a7819
Stamatios Aleiferis <stamatios.aleiferis@tufts.edu>
2020-03-19 23:04:30 -0400

    staff add done

--- routes/staff.js
+++ routes/staff.js
@@ -15,7 +15,33 @@ router.post('/add', async (req, res) => {
     const passwordHash = await bcrypt.hash(req.body.password, HASH_COST);
     try {
         const staffAdded = await req.db.collection("Staff").insert({
-            name, email, grade, phone, passwordHash, verificationToken
+            name, email, phone, passwordHash, verificationToken
+        });
+        await req.db.collection("User").insert({
+            email, passwordHash
+        });
+        res.json({
+            message: 'Successfully added '+ role,
+            staff: staffAdded
+        });
+    } catch (e) {
+        res.json({
+            message: 'Failed adding' + role
+        });
+    }
+});
+
+router.post('/sendEmail', async (req, res) => {
+    const email = req.body.email;
+    const name = req.body.name;
+    const role = req.body.role;
+    const phone = req.body.phone;
+    // TODO: for email-based verification
+    const verificationToken = crypto({length: 16});
+    const passwordHash = await bcrypt.hash(req.body.password, HASH_COST);
+    try {
+        const staffAdded = await req.db.collection("Staff").insert({
+            name, email, phone, passwordHash, verificationToken
         });
         await req.db.collection("User").insert({
             email, passwordHash
