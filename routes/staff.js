24d70653a01b37fcc6a76f8213064487bdfb961d
Stamatios Aleiferis <stamatios.aleiferis@tufts.edu>
2020-03-20 23:48:48 -0400

    sendEmail api init

--- routes/staff.js
+++ routes/staff.js
@@ -32,29 +32,31 @@ router.post('/add', async (req, res) => {
 });
 
 router.post('/sendEmail', async (req, res) => {
-    const email = req.body.email;
-    const name = req.body.name;
-    const role = req.body.role;
-    const phone = req.body.phone;
+    const email = req.body.email; //Get current email
+    const grades = req.body.grades;
+    const subject = req.body.subject;
+    const body = req.body.body;
+
+
+
     // TODO: for email-based verification
-    const verificationToken = crypto({length: 16});
-    const passwordHash = await bcrypt.hash(req.body.password, HASH_COST);
+    //const verificationToken = crypto({length: 16});
+    //const passwordHash = await bcrypt.hash(req.body.password, HASH_COST);
     try {
-        const staffAdded = await req.db.collection("Staff").insert({
-            name, email, phone, passwordHash, verificationToken
-        });
-        await req.db.collection("User").insert({
-            email, passwordHash
-        });
+        const dbData = await req.db.collection("Student").find({grade: grades}).project({email:1}).toArray();
         res.json({
-            message: 'Successfully added '+ role,
-            staff: staffAdded
-        });
+          message:'Success'
+        })
+        console.log(dbData)
     } catch (e) {
+      console.log(e)
         res.json({
-            message: 'Failed adding' + role
+            message: 'Failed'
         });
     }
+    res.send()
+    //TODO: send email to address in dbData[i]["email"]
+
 });
 
 module.exports = router;
